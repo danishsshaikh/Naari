3b0937c2bb7cac01fabd88b1fd8a346e97f0e634
danishsshaikh <danishsshaikh@gmail.com>
2020-04-30 14:55:56 +0530

    :see_no_evil: | Add Relative
    Relative members to be added scene done, with the listView.
    Please check .java and .xml file.

---
+++ app/src/main/java/com/dan/naari/AddRelative.java
@@ -0,0 +1,52 @@
+package com.dan.naari;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class AddRelative extends AppCompatActivity {
+
+    EditText et1;
+    EditText et2;
+    Button bt;
+    ArrayList<String> arrayList;
+    ArrayAdapter<String> adapter;
+    ListView lv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_relative);
+
+        et1 = (EditText) findViewById(R.id.editText2);
+        et2 = (EditText) findViewById(R.id.editText3);
+        bt = (Button) findViewById(R.id.button);
+        lv = (ListView) findViewById(R.id.listView);
+
+        arrayList = new ArrayList<String>();
+        adapter = new ArrayAdapter<String>(AddRelative.this,
+                android.R.layout.simple_list_item_1, arrayList);
+        // Set ListView Adapter
+        lv.setAdapter(adapter);
+        onBtnClick();
+
+    }
+
+    public void onBtnClick(){
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = et1.getText().toString().trim();
+                String number = et2.getText().toString().trim();
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+}
