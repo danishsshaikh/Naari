ae153c4fbfead746f6ed1fedfdb893a3e869f3a0
danishsshaikh <danishsshaikh@gmail.com>
2020-04-30 14:57:40 +0530

    :see_no_evil: | Main Menu Scene
    @aryanveturekar Waiting for the UI files, to get started with the .xml file.

---
+++ app/src/main/java/com/dan/naari/MainScreen.java
@@ -0,0 +1,14 @@
+package com.dan.naari;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}
