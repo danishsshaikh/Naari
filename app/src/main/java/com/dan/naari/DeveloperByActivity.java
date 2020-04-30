e441ec8987a08d0abcc5d7cb184e73c0855fd724
danishsshaikh <danishsshaikh@gmail.com>
2020-05-01 03:07:47 +0530

    🙈 | Design Done, Intent Remaining.

---
+++ app/src/main/java/com/dan/naari/DeveloperByActivity.java
@@ -0,0 +1,14 @@
+package com.dan.naari;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeveloperByActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_developer_by);
+    }
+}
