7c54d4140252e547c3a1affb47a765cecaa4023e
danishsshaikh <danishsshaikh@gmail.com>
2020-05-01 06:15:26 +0530

    🙈 | Portfolios added
    Added OnClick function with Intents
    
    Co-Authored-By: Aryan Veturekar <aryanveturekar@users.noreply.github.com>
    Co-Authored-By: Himanshu Singh <himanshusiingh@users.noreply.github.com>

--- app/src/main/java/com/dan/naari/DeveloperByActivity.java
+++ app/src/main/java/com/dan/naari/DeveloperByActivity.java
@@ -2,7 +2,10 @@ package com.dan.naari;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class DeveloperByActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class DeveloperByActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_developer_by);
+    }
+
+    public void Dan(View v){
+        Intent implicit = new Intent(Intent.ACTION_VIEW, Uri.parse("https://danishsshaikh.github.io/"));
+        startActivity(implicit);
+    }
+
+    public void Ary(View v){
+        Intent implicit = new Intent(Intent.ACTION_VIEW, Uri.parse("https://aryanveturekar.github.io/"));
+        startActivity(implicit);
+
+    }
+
+    public void Him(View v){
+        Intent implicit = new Intent(Intent.ACTION_VIEW, Uri.parse("https://himanshusiingh.github.io/"));
+        startActivity(implicit);
+
     }
 }
