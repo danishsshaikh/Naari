e76d5365d4e7b2f0b5724eab0c7484d2cb91b16e
danishsshaikh <danishsshaikh@gmail.com>
2020-05-01 06:14:43 +0530

    🙈 | Main Menu scene migrate to MainActivity
    Also, few minor changes in the .xml files of trigger, and helpline call
    
    Co-Authored-By: Aryan Veturekar <aryanveturekar@users.noreply.github.com>
    Co-Authored-By: Himanshu Singh <himanshusiingh@users.noreply.github.com>

--- app/src/main/java/com/dan/naari/MainActivity.java
+++ app/src/main/java/com/dan/naari/MainActivity.java
@@ -2,7 +2,9 @@ package com.dan.naari;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+//    public void myProfile(View v){
+//        Intent i = new Intent(getApplicationContext(), MyProfile.class);
+//        startActivity(i);
+//    }
+
+    public void addRelative(View v){
+        Intent i = new Intent(getApplicationContext(), AddRelative.class);
+        startActivity(i);
+    }
+
+    public void helplineNumbers(View v){
+        Intent i = new Intent(getApplicationContext(), helplineCall.class);
+        startActivity(i);
+    }
+
+    public void triggers(View v){
+        Intent i = new Intent(getApplicationContext(), TrigActivity.class);
+        startActivity(i);
+    }
+
+    public void developedBy(View v){
+        Intent i = new Intent(getApplicationContext(), DeveloperByActivity.class);
+        startActivity(i);
+    }
+
+    public void HowTo(View v){
+        Intent i = new Intent(getApplicationContext(), HowToSwipe.class);
+        startActivity(i);
+    }
+
+
+
+
 }
