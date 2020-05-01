756a6c6736bdf7fd29271b7ff07e031aa5171e82
Himanshu Singh <55758503+himanshusiingh@users.noreply.github.com>
2020-05-01 17:56:21 +0530

    :japanese_goblin:  / Splash.java , Registration.java, Login.java

---
+++ app/src/main/java/com/dan/naari/Registration.java
@@ -0,0 +1,96 @@
+package com.dan.naari;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Registration extends AppCompatActivity {
+    EditText mFullName,mEmail,mPassword,mPhone;
+    Button mRegisterbtn;
+    TextView mCreateText;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        mFullName = findViewById(R.id.fullName);
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.password);
+        mPhone = findViewById(R.id.phone);
+        mRegisterbtn = findViewById(R.id.registerbtn);
+        mCreateText = findViewById(R.id.createText);
+
+        fAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBar);
+
+        if (fAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+        mRegisterbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+                if (TextUtils.isEmpty(email)) {
+                    mEmail.setError("Email is Required.");
+                    return;
+
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    mPassword.setError("Password is Required");
+                    return;
+
+                }
+                if(password.length() < 6) {
+                    mPassword.setError("Password Must be >= 6 Characters");
+                    return;
+                }
+                progressBar.setVisibility(View.VISIBLE);
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Registration.this, "User Created.", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+                        } else {
+                            Toast.makeText(Registration.this, "Error" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+
+
+            }
+        });
+
+        mCreateText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Login.class));
+            }
+        });
+
+    }
+}
+
--- app/src/main/java/com/dan/naari/Splash.java
+++ app/src/main/java/com/dan/naari/Splash.java
@@ -2,7 +2,9 @@ package com.dan.naari;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splash extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, Registration.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
     }
 }
