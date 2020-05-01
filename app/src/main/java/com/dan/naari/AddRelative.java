24ef7621a53e7eb07872c580ccdc2d7d4ad50e01
danishsshaikh <danishsshaikh@gmail.com>
2020-05-01 21:44:45 +0530

    Trigger|

--- app/src/main/java/com/dan/naari/AddRelative.java
+++ app/src/main/java/com/dan/naari/AddRelative.java
@@ -1,21 +1,28 @@
 package com.dan.naari;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.sqlite.SQLiteOpenHelper;
 //import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import java.util.Arrays;
 import java.util.regex.Pattern;
 
 public class AddRelative extends AppCompatActivity {
 
+    private static final int REQUEST_CALL = 1;
     DatabaseHelper myDB;
     Button btnAdd,btnView;
     EditText editText, editText2;
@@ -65,6 +72,54 @@ public class AddRelative extends AppCompatActivity {
         }else{
             Toast.makeText(this, "Something went wrong :(.", Toast.LENGTH_LONG).show();
         }
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+
+        int action, keycode;
+
+        action = event.getAction();
+        keycode = event.getKeyCode();
+
+        switch (keycode)
+        {
+            case KeyEvent.KEYCODE_VOLUME_UP:
+                {
+                if(KeyEvent.ACTION_UP == action){
+//                    count++;
+                    //String S1 = String.valueOf(count);
+                    //Log.d("upButton", S1);
+                    Intent callIntent = new Intent(Intent.ACTION_CALL);
+                    callIntent.setData(Uri.parse("tel:9137485752"));
+                    if (ActivityCompat.checkSelfPermission(AddRelative.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(AddRelative.this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+
+                    } else {
+                        callIntent.setData(Uri.parse("tel: 9137485752"));
+                        startActivity(callIntent);
+                    }
+                    startActivity(callIntent);
+                }
+            }
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+                if(KeyEvent.ACTION_DOWN == action){
+                    //count = 0;
+                    //String S2 = String.valueOf(count);
+                    //Log.d("downButton", S2);
+                    Intent callIntent = new Intent(Intent.ACTION_CALL);
+                    callIntent.setData(Uri.parse("tel:9137485752"));
+                    if (ActivityCompat.checkSelfPermission(AddRelative.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(AddRelative.this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+
+                    } else {
+                        callIntent.setData(Uri.parse("tel: 9137485752"));
+                        startActivity(callIntent);
+                    }
+                    startActivity(callIntent);
+                }
+        }
+        return super.dispatchKeyEvent(event);
     }
 }
 
